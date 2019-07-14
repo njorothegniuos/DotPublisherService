8b43d752440749217d11723b61ab8183149f58ea
njorothegniuos <njosh51@gmail.com>
2019-07-14 03:01:18 +0300

    First commit

---
+++ src/main/java/com/MBASE/DotPublisherServiceApplication.java
@@ -0,0 +1,13 @@
+package com.MBASE;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DotPublisherServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DotPublisherServiceApplication.class, args);
+	}
+
+}
